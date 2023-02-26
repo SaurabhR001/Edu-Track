62acd7d2be40cc44ff1820974eb28fbd27ebf399
yashmahajan29 <yashsmahajan82@gmail.com>
2023-02-26 06:55:34 +0530

    fixed data fetch for full name

--- app/src/main/java/com/example/edutrack/MainActivity.java
+++ app/src/main/java/com/example/edutrack/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.edutrack;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.nio.file.FileStore;
 
 public class MainActivity extends AppCompatActivity {
+    TextView fullName;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(getApplicationContext(),Login.class));
                 finish();
+            }
+        });
+
+        fullName = findViewById(R.id.profileName);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("Users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                fullName.setText(documentSnapshot.getString("FullName"));
             }
         });
     }
