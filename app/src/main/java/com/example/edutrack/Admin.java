792508f5ac66dfb02b795d738fa8d1fabde5b038
yashmahajan29 <yashsmahajan82@gmail.com>
2023-02-26 06:24:55 +0530

    fixed admin logout

--- app/src/main/java/com/example/edutrack/Admin.java
+++ app/src/main/java/com/example/edutrack/Admin.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,11 +15,14 @@ public class Admin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-    }
-
-    public void logoutAdmin(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
+        Button logout = findViewById(R.id.button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+            }
+        });
     }
 }
