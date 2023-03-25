ed34c5297a1296249f5f913d2a192380ebafb927
yashmahajan29 <yashsmahajan82@gmail.com>
2023-03-26 02:39:39 +0530

    Ready to edit

--- app/src/main/java/com/example/edutrack/StudentDashboard.java
+++ app/src/main/java/com/example/edutrack/StudentDashboard.java
@@ -17,81 +17,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class StudentDashboard extends AppCompatActivity {
-    TextView fullName,phone,ccCode;
-    FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
-    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
-        Button logout = findViewById(R.id.logoutBtn);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),Login.class));
-                finish();
-            }
-        });
-
-        Button edit = findViewById(R.id.editBtn);
-
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),studEdit.class));
-            }
-        });
-
-
-
-
-
-
-
-//        Button logout = findViewById(R.id.logoutBtn);
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FirebaseAuth.getInstance().signOut();
-//                startActivity(new Intent(getApplicationContext(),Login.class));
-//                finish();
-//            }
-//        });
-
-
-
-        fullName = findViewById(R.id.profileName);
-        phone = findViewById(R.id.profilePhone);
-//        ccCode = findViewById(R.id.cc_code);
-
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
-
-        userId = fAuth.getCurrentUser().getUid();
-
-        DocumentReference documentReference = fStore.collection("Users").document(userId);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-                fullName.setText(documentSnapshot.getString("FullName"));
-                phone.setText(documentSnapshot.getString("PhoneNumber"));
-                //ccCode.setText(documentSnapshot.getString("CC_Code"));
-            }
-        });
-//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-//                phone.setText(documentSnapshot.getString("PhoneNumber"));
-//            }
-//        });
-//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-//                ccCode.setText(documentSnapshot.getString("CC_Code"));
-//            }
-//        });
     }
 }
